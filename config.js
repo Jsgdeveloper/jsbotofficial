26c31f67e235d6b1d74cf04c5628bfc356c62831
Jsgdeveloper <jsgdeveloperofficial@gmail.com>
2024-10-23 03:06:43 +0700

    JamaSantuy Gamers

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 // config.js
 
 const config = {
-    botName: "HenAi Bot", // Nama bot yang akan ditampilkan
-    adminId: "YOUR_ADMIN_ID", // Ganti dengan ID admin yang valid
+    botName: "JsBot", // Nama bot yang akan ditampilkan
+    adminId: "1250940447325421663", // Ganti dengan ID admin yang valid
     apiKey: "YOUR_API_KEY", // Ganti dengan API Key jika diperlukan
     prefixes: ["!", "?", "$", "/"], // Daftar prefix yang didukung
 };
